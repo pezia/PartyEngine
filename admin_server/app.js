ae7432905ecf6ac00012d5057aa12f9e644634b1
Zsolt Petrik <pezia@pezia.hu>
2015-04-06 19:01:36 +0200

    Added skeleton admin server app

---
+++ admin_server/app.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var winston = require('winston');
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+io.on('connection', function (socket) {
+    winston.info('User connected. Socket ID: ' + socket.id);
+});
+
+http.listen(3000, function () {
+    winston.info('Demo Party admin server listaning on port 3000');
+});
